a22071da7409225c4c5ea30fb364272fda1e427a
experthoon <kimkt1377@naver.com>
2025-06-10 23:20:54 +0900

    [SCRUM-5] 로그인 페이지 및 초기 설정

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+//import App from './App.vue'
+import LoginView from './view/LoginView.vue'
 
-createApp(App).mount('#app')
+createApp(LoginView).mount('#app')
---
+++ src/view/LoginView.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <div>
+        <img alt="Vue logo" src="@/assets/YtaxiLogo.png">
+        <h2>로그인</h2>
+        <div id="loginForm"> 
+            <p>
+              <input class="w3-input" name="uid" placeholder="ID를 입력해주세요" ><br>
+            </p>
+            <p>
+              <input name="password" class="w3-input" placeholder="PW를 입력해주세요" type="password">
+            </p>
+            <p>
+              <button type="submit" class="w3-button w3-green w3-round">로그인</button>
+              <button type="submit" class="w3-button w3-green w3-round">회원가입</button>
+            </p>
+        </div>
+      </div>
+    </div>
+  </template>
+
+  <script>
+
+ </script>
